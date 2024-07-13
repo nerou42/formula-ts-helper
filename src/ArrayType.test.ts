81a8c68ca72cd94784ba6ef75a3f012abddad16b
TimoLehnertz <57093983+TimoLehnertz@users.noreply.github.com>
2024-07-13 10:09:42 +0200

    Fixed readme

--- src/ArrayType.test.ts
+++
@@ -1,8 +0,0 @@
-import { ArrayType } from "./ArrayType";
-import { NeverType } from "./NeverType";
-
-describe('ArrayType', () => {
-  it('should create an ArrayType', () => {
-    expect(new ArrayType(new NeverType(), new NeverType())).toBeInstanceOf(ArrayType);
-  });
-});
